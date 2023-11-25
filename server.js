ef70c0a207093ef9232f00f3e685ad8f673ae2d4
DSaddam <saddamhusain4081316@gmail.com>
2023-11-25 11:09:10 +0530

    frameAncestors added

--- server.js
+++ server.js
@@ -39,15 +39,14 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.options("*", cors());
-// app.use(
-//   helmet.contentSecurityPolicy({
-//     directives: {
-//       // other directives
-//       frameAncestors: ["'admin.shopify.com'"] // Compliant
-//     }
-//   })
-// );
-// ...
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      // other directives
+      frameAncestors: ["'admin.shopify.com'"] // Compliant
+    }
+  })
+);
 
 app.use((req, res, next) => {
   const shop = req.query.shop;
